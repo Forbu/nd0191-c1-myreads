9932f58984da30a629b3ddbd56646301129d6344
Adrien B <forbu14@gmail.com>
2024-11-12 23:00:06 +0100

    feat: correction on handling the non image available case or the author non available case

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -1,40 +1,43 @@
 const Book = (props) => {
-
+    const { book } = props;
+    
     return <div className="book">
-    <div className="book-top">
-      <div
-        className="book-cover"
-        style={{
-          width: 128,
-          height: 193,
-          backgroundImage:
-            `url(${props.book.imageLinks.smallThumbnail})`,
-        }}
-      ></div>
-      <div className="book-shelf-changer">
-        <select 
-        value={props.book.shelf || 'none'} 
-        onChange={(event) => props.addbook(props.book, event.target.value)}>
-          <option value="move" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading" disabled={props.book.shelf === 'currentlyReading'}>
-            Currently Reading
-          </option>
-          <option value="wantToRead" disabled={props.book.shelf === 'wantToRead'}>
-            Want to Read
-          </option>
-          <option value="read" disabled={props.book.shelf === 'read'}>
-            Read
-          </option>
-          <option value="none" disabled={props.book.shelf === 'none'}>
-            None
-          </option>
-        </select>
-      </div>
-    </div>
-    <div className="book-title">{props.book.title}</div>
-        <div className="book-authors">{props.book.authors}</div>
+        <div className="book-top">
+            <div
+                className="book-cover"
+                style={{
+                    width: 128,
+                    height: 193,
+                    backgroundImage:
+                        book.imageLinks?.smallThumbnail ? 
+                        `url(${book.imageLinks.smallThumbnail})` :
+                        'none',
+                }}
+            ></div>
+            <div className="book-shelf-changer">
+                <select 
+                    value={book.shelf || 'none'} 
+                    onChange={(event) => props.addbook(book, event.target.value)}>
+                    <option value="move" disabled>
+                        Move to...
+                    </option>
+                    <option value="currentlyReading" disabled={book.shelf === 'currentlyReading'}>
+                        Currently Reading
+                    </option>
+                    <option value="wantToRead" disabled={book.shelf === 'wantToRead'}>
+                        Want to Read
+                    </option>
+                    <option value="read" disabled={book.shelf === 'read'}>
+                        Read
+                    </option>
+                    <option value="none" disabled={book.shelf === 'none'}>
+                        None
+                    </option>
+                </select>
+            </div>
+        </div>
+        <div className="book-title">{book.title}</div>
+        {book.authors && <div className="book-authors">{book.authors}</div>}
     </div>;
 };
 
